c5d3a06a3d450cf416a0460f698e81b2a05e8438
melany-rios <136763956+melany-rios@users.noreply.github.com>
2026-09-23 15:51:52 -0300

    Add fictional courses and students data
    Added fictional data for courses and students.

--- data/datos_prueba.py
+++ data/datos_prueba.py
@@ -1,1 +1,122 @@
+# ==========================================
+# DATOS FICTICIOS - AULA360
+# ==========================================
 
+# Cursos y materias
+cursos = [
+    {
+        "id": 1,
+        "curso": "5° 1°",
+        "materia": "Sistemas Informáticos"
+    },
+    {
+        "id": 2,
+        "curso": "5° 1°",
+        "materia": "Software II"
+    },
+        {
+        "id": 3,
+        "curso": "4° 1°",
+        "materia": "Software I"
+    },
+        {
+        "id": 4,
+        "curso": "4° 2°",
+        "materia": "Hardware II"
+    }
+]
+
+
+# Alumnos ficticios
+alumnos = [
+    {
+        "id": 1,
+        "nombre": "Sofía",
+        "apellido": "Gómez",
+        "curso_id": 1
+    },
+    {
+        "id": 2,
+        "nombre": "Mateo",
+        "apellido": "López",
+        "curso_id": 1
+    },
+    {
+        "id": 3,
+        "nombre": "Valentina",
+        "apellido": "Pérez",
+        "curso_id": 1
+    },
+    {
+        "id": 4,
+        "nombre": "Tomás",
+        "apellido": "Rodríguez",
+        "curso_id": 1
+    },
+    {
+        "id": 5,
+        "nombre": "Camila",
+        "apellido": "Fernández",
+        "curso_id": 2
+    },
+    {
+        "id": 6,
+        "nombre": "Lucas",
+        "apellido": "Martínez",
+        "curso_id": 2
+    },
+    {
+        "id": 7,
+        "nombre": "Martina",
+        "apellido": "Díaz",
+        "curso_id": 2
+    },
+    {
+        "id": 8,
+        "nombre": "Joaquín",
+        "apellido": "Sánchez",
+        "curso_id": 3
+    },
+    {
+        "id": 9,
+        "nombre": "Agustina",
+        "apellido": "Romero",
+        "curso_id": 3
+    },
+    {
+        "id": 10,
+        "nombre": "Franco",
+        "apellido": "Torres",
+        "curso_id": 4
+    },
+    {
+        "id": 11,
+        "nombre": "Micaela",
+        "apellido": "Ruiz",
+        "curso_id": 4
+    },
+    {
+        "id": 12,
+        "nombre": "Nicolás",
+        "apellido": "Acosta",
+        "curso_id": 4
+    },
+    {
+        "id": 13,
+        "nombre": "Julieta",
+        "apellido": "Vega",
+        "curso_id": 4
+    },
+    {
+        "id": 14,
+        "nombre": "Benjamín",
+        "apellido": "Navarro",
+        "curso_id": 4
+    },
+    {
+        "id": 15,
+        "nombre": "Renata",
+        "apellido": "Castro",
+        "curso_id": 4
+    }
+]
